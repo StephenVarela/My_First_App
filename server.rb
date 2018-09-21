7f6f13c55d0f725815bd7bb34cc93e55882a29dd
StephenVarela <varelast92@gmail.com>
2018-09-20 23:28:33 -0400

    finished routes  for favorites and redirects

--- server.rb
+++ server.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 
+get '/' do
+  redirect to ('/home')
+end
+
+get '/gallery' do
+  redirect to ('/portfolio')
+end
+
 get '/home' do
   erb :index
 end
@@ -12,4 +20,9 @@ get '/about_me' do
   @skills = ['git', 'HTML', 'CSS', 'Ruby']
   @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about_me
+end
+
+get '/favorites' do
+  @fav_links = ['http://google.com', 'http://facebook.com', 'http://youtube.com', 'http://twitter.com', 'http://linkedin.com']
+  erb :favorites
 end
